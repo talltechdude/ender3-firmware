10e9b0b1b7b2d3ef6f07f89f9f086ea454cc6899
TallTechDude <talltechdude@gmail.com>
2021-07-28 19:20:14 +1000

    build: troubleshooting build chain

--- Configuration_pre.h
+++ Configuration_pre.h
@@ -1,20 +1,28 @@
+//===========================================================================
+//========================= TTD Pre Configuration ===========================
+//===========================================================================
+
 #define ENDER3
-#define ENDER3_STOCK_FRAME
-#define BLTOUCH
-#define SERVO0_PIN P2_04
-#define CUSTOM_PROBE
-#define EZABL_PROBE_EDGE 35
-#define EZABL_POINTS 3
-#define HEATERS_ON_DURING_PROBING
-#define FINE_BABYSTEPPING
-#define STOCK_HOTEND_THERMISTOR
-#define STOCK_BED_THERMISTOR
-#define ENABLE_PIDBED
-#define DIRECT_DRIVE_PRINTER
-#define CUSTOM_ESTEPS
-#define CUSTOM_ESTEPS_VALUE 139
-#define STEALTHCHOP_E
-#define LINEAR_ADVANCE
-#define LINEAR_ADVANCE_K 0.05
-#define LCD_LANGUAGE en
-#define NOZZLE_TO_PROBE_OFFSET {-43,-13,0}
+// #define ENDER3_STOCK_FRAME
+// #define BLTOUCH
+// #define SERVO0_PIN P2_04
+// #define CUSTOM_PROBE
+// #define EZABL_PROBE_EDGE 35
+// #define EZABL_POINTS 3
+// #define HEATERS_ON_DURING_PROBING
+// #define FINE_BABYSTEPPING
+// #define STOCK_HOTEND_THERMISTOR
+// #define STOCK_BED_THERMISTOR
+// #define ENABLE_PIDBED
+// #define DIRECT_DRIVE_PRINTER
+// #define CUSTOM_ESTEPS
+// #define CUSTOM_ESTEPS_VALUE 139
+// #define STEALTHCHOP_E
+// #define LINEAR_ADVANCE
+// #define LINEAR_ADVANCE_K 0.05
+// #define LCD_LANGUAGE en
+// #define NOZZLE_TO_PROBE_OFFSET {-43,-13,0}
+
+//===========================================================================
+//====================== TTD Pre Configuration End ==========================
+//===========================================================================
