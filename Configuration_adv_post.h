488281d852d99dd0cb53aa262f1986e6aaee8587
TallTechDude <talltechdude@gmail.com>
2021-07-29 20:23:08 +1000

    feat(marlin): custom post Configuration_adv.h changes

---
+++ Configuration_adv_post.h
@@ -0,0 +1,22 @@
+//===========================================================================
+//====================== TTD ADV Post Configuration =========================
+//===========================================================================
+
+#pragma once
+
+// TallTechDude Customisations ---------------------
+
+#undef FAN_PIN
+#undef E0_AUTO_FAN_PIN
+
+#define FAN_PIN P1_22
+#define E0_AUTO_FAN_PIN FAN_PIN
+
+#undef USE_CONTROLLER_FAN
+#undef CONTROLLER_FAN_PIN
+#undef CONTROLLER_FAN_EDITABLE
+#undef CONTROLLER_FAN_MENU
+
+//===========================================================================
+//==================== TTD ADV Post Configuration End =======================
+//===========================================================================
