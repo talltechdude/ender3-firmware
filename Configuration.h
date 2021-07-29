05b3685c34c7d303ab9e89dfaf55b54c91f34cc0
TallTechDude <talltechdude@gmail.com>
2021-07-29 10:09:35 +1000

    feat(marlin): setup some basic defaults

--- Configuration.h
+++ Configuration.h
@@ -5,16 +5,19 @@
 
 #define ENDER3
 #define ENDER3_STOCK_FRAME
+#define CUSTOM_PRINTER_NAME
+#define USER_PRINTER_NAME "TallTechDude E3"
 #define BLTOUCH
 #define SERVO0_PIN P2_04
 #define CUSTOM_PROBE
 #define EZABL_PROBE_EDGE 35
 #define EZABL_POINTS 3
 #define HEATERS_ON_DURING_PROBING
+#define PROBE_OFFSET_WIZARD
 #define FINE_BABYSTEPPING
 #define STOCK_HOTEND_THERMISTOR
 #define STOCK_BED_THERMISTOR
-#define ENABLE_PIDBED
+//#define ENABLE_PIDBED
 #define DIRECT_DRIVE_PRINTER
 #define CUSTOM_ESTEPS
 #define CUSTOM_ESTEPS_VALUE 139
@@ -22,7 +25,11 @@
 #define LINEAR_ADVANCE
 #define LINEAR_ADVANCE_K 0.05
 #define LCD_LANGUAGE en
-#define NOZZLE_TO_PROBE_OFFSET {-43,-13,0}
+#define NOZZLE_TO_PROBE_OFFSET {-43,-13,-2.57}
+
+#define DEFAULT_Kp 36.17
+#define DEFAULT_Ki 4.23
+#define DEFAULT_Kd 77.34
 
 //===========================================================================
 //========================= TTD Configuration End ===========================
